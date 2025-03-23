5f7b33f96dec2558e0681b5fc9500ecbfee989f8
Mona Lisa <hieutaihou@gmail.com>
2025-03-23 22:06:40 +0700

    aa

--- iconic-fe/vite.config.ts
+++ iconic-fe/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-
+import legacy from "@vitejs/plugin-legacy";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"], // Polyfill cho async/await
+    }),
+  ],
   server: {
     port: 3000,
     allowedHosts: [".ngrok-free.app"],
