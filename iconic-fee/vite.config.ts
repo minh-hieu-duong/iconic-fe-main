7e480a4e9857cbe1e1b0e883a735699274865449
Mona Lisa <hieutaihou@gmail.com>
2025-03-23 23:27:02 +0700

    aa

--- iconic-fee/vite.config.ts
+++ iconic-fee/vite.config.ts
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: "es2019", // Hoặc thử es2018 nếu vẫn lỗi
+  },
+  server: {
+    hmr: false, // Tắt Hot Module Reloading (có thể gây lỗi trên Safari cũ)
+  },
 });
