2e394f57865eeabbe63a0a9f3e8aa681664a1fba
Mona Lisa <hieutaihou@gmail.com>
2025-03-23 22:49:39 +0700

    a

--- iconic-fe/vite.config.ts
+++ iconic-fe/vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import legacy from "@vitejs/plugin-legacy";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     legacy({
-      targets: ["defaults", "not IE 11"],
-      additionalLegacyPolyfills: ["regenerator-runtime/runtime"], // Polyfill cho async/await
+      targets: ["safari >= 13", "not IE 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
     }),
   ],
   server: {
