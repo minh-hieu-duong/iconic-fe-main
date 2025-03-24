23b9dba2ebc215d8c6a5e8a73c32a7f719bdf47d
Mona Lisa <hieutaihou@gmail.com>
2025-03-24 20:26:47 +0700

    aa

--- iconic-fe/vite.config.ts
+++ iconic-fe/vite.config.ts
@@ -1,28 +1,15 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import legacy from "@vitejs/plugin-legacy";
-import autoprefixer from "autoprefixer";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    legacy({
-      targets: ["safari >= 12", "not IE 11"], // Cập nhật targets để bao gồm Safari 13
-      additionalLegacyPolyfills: ["regenerator-runtime/runtime"], // Polyfill cho async/await
-    }),
-  ],
-  css: {
-    postcss: {
-      plugins: [autoprefixer()],
-    },
-  },
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     allowedHosts: [".ngrok-free.app"],
   },
   build: {
-    target: "es2019",
+    manifest: true,
   },
 });
